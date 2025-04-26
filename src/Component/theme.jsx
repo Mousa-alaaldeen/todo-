38dca1cded0afbf949b92db29931a3f924c265c3
musa azmi <azmimusa395@gmail.com>
2025-04-26 21:24:42 +0300

    font-family

---
+++ src/Component/theme.jsx
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Inter, sans-serif', // أو Roboto أو Nunito حسب اختيارك
+  },
+  
+ 
+});
+
+export default theme;
